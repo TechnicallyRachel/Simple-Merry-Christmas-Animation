3c8fb7788fa3fd0367f007510e163e37439846ed
Rachel <paintmetothemoon@gmail.com>
2017-11-06 13:45:52 -0800

    uploading code
    Merry Christmas simple animation code

---
+++ FADrawing1.py
@@ -0,0 +1,15 @@
+import time
+while True:
+    print('Merry★* 。 • ˚ ˚ ˛ ˚ ˛ •')
+    time.sleep(0.5)
+    print('•。★Christmas★ 。* 。')
+    time.sleep(0.5)
+    print('° 。 ° ˛˚˛ * _Π_____*。*˚')
+    time.sleep(0.5)
+    print('˚ ˛ •˛•˚ */______/~＼。˚ ˚ ˛')
+    time.sleep(0.5)
+    print('˚ ˛ •˛• ˚ ｜ 田田 ｜門｜ ˚')
+    time.sleep(0.5)
+    print('And a happy new year!')
+    
+
